f2b092ea48eb07951a024fb3b0d992bdf0892f6d
Andrei Tupitcyn <andrew.tupitsin@gmail.com>
2016-05-20 17:16:15 +0300

    feat: add SequenceFile source and sink

---
+++ spark/src/main/scala/com/github/andr83/parsek/spark/sink/SequenceFileSink.scala
@@ -0,0 +1,91 @@
+package com.github.andr83.parsek.spark.sink
+
+import java.util.UUID
+
+import com.github.andr83.parsek._
+import com.github.andr83.parsek.formatter.FieldFormatter
+import com.github.andr83.parsek.serde.{SerDe, Serializer, StringSerializer}
+import com.github.andr83.parsek.spark.util.RDDUtils.{DefaultPartitioner, FieldsPartitioner}
+import com.github.andr83.parsek.spark.util.{HadoopUtils, RDDUtils}
+import com.typesafe.config.Config
+import net.ceedubs.ficus.Ficus._
+import org.apache.hadoop.io.NullWritable
+import org.apache.hadoop.io.compress.CompressionCodec
+import org.apache.hadoop.mapred.lib.MultipleSequenceFileOutputFormat
+import org.apache.spark.rdd.RDD
+
+import scala.collection.JavaConversions._
+
+/**
+  * @author andr83
+  */
+case class SequenceFileSink(
+  path: String,
+  codec: Option[Class[_ <: CompressionCodec]],
+  serializer: () => Serializer,
+  partitions: Seq[FieldFormatter] = Seq.empty[FieldFormatter],
+  numPartitions: Option[Int] = None,
+  fileNamePattern: Option[String] = None
+) extends Sink {
+
+  def this(config: Config) = this(
+    path = config.getString("path"),
+    codec = config.as[Option[String]]("codec") map HadoopUtils.getCodec,
+    serializer = config.as[Option[Config]]("serializer")
+      .map(serializerConf => () => SerDe(serializerConf))
+      .getOrElse(StringSerializer.factory),
+    partitions = if (config.hasPath("partitions"))
+      FieldFormatter(config.getList("partitions"))
+    else Seq.empty[FieldFormatter],
+    numPartitions = config.as[Option[Int]]("numPartitions"),
+    fileNamePattern = config.as[Option[String]]("fileNamePattern")
+  )
+
+  override def sink(rdd: RDD[PValue], time: Long): Unit = {
+    try {
+      if (partitions.nonEmpty || fileNamePattern.nonEmpty) {
+        val pattern = fileNamePattern map (pattern => {
+          pattern
+            .replaceAllLiterally("${randomUUID}", UUID.randomUUID().toString)
+            .replaceAllLiterally("${timeInMs}", time.toString)
+        })
+
+        val partitioner = if (partitions.isEmpty) DefaultPartitioner(pattern)
+        else FieldsPartitioner(partitions, pattern)
+
+        RDDUtils
+          .serializeAndPartitionBy(rdd, serializer, partitioner, numPartitions)
+          .saveAsHadoopFile(
+            path,
+            classOf[NullWritable],
+            classOf[String],
+            classOf[SequenceFileSink.RDDMultipleSequenceOutputFormat],
+            codec = codec
+          )
+      } else {
+        RDDUtils
+          .serialize(rdd, serializer)
+          .map(v=> NullWritable.get() -> v)
+          .saveAsSequenceFile(path, codec)
+      }
+    } catch {
+      case e: Exception =>
+        logger.error(e.toString, e)
+    }
+  }
+}
+
+
+object SequenceFileSink {
+
+  class RDDMultipleSequenceOutputFormat extends MultipleSequenceFileOutputFormat[Any, Any] {
+
+    override def generateActualKey(key: Any, value: Any): Any =
+      NullWritable.get()
+
+    override def generateFileNameForKeyValue(key: Any, value: Any, name: String): String = {
+      key.asInstanceOf[String]
+    }
+  }
+
+}
---
+++ spark/src/main/scala/com/github/andr83/parsek/spark/source/SequenceFileSource.scala
@@ -0,0 +1,41 @@
+package com.github.andr83.parsek.spark.source
+
+import com.github.andr83.parsek.spark.PathFilter._
+import com.github.andr83.parsek.spark.{PathFilter, SparkJob}
+import com.github.andr83.parsek.{PString, PValue}
+import com.typesafe.config.Config
+import net.ceedubs.ficus.Ficus._
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.Text
+import org.apache.spark.rdd.RDD
+
+/**
+  * @author andr83
+  */
+case class SequenceFileSource(path: Seq[String], filters: Seq[PathFilter]) extends Source {
+
+  def this(config: Config) = this(
+    path = config.getAnyRef("path") match {
+      case list: List[_] => list.map(_.toString)
+      case value => List(value.toString)
+    },
+    filters = config.as[Option[List[Config]]]("filters") map (filters => {
+      filters.map(f => PathFilter(f))
+    }) getOrElse Seq.empty[PathFilter]
+  )
+
+  // Default filter to exclude all files with underscore prefix like _SUCCESS
+  val defaultHadoopFilter = (p: Path) => !p.getName.startsWith("_")
+
+
+  override def apply(job: SparkJob): RDD[PValue] = {
+    val files = path.flatMap(job.listFilesOnly(_, defaultHadoopFilter +: filters))
+    logger.info("Input files:")
+    files.sorted.foreach(f => logger.info(f))
+    if (files.isEmpty) {
+      job.sc.emptyRDD[PValue]
+    } else {
+      job.sc.sequenceFile(files.mkString(","), classOf[String], classOf[Text]).map(kv=> PString(kv._2.toString))
+    }
+  }
+}
