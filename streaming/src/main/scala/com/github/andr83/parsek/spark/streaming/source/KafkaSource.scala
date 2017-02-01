30f70513d4abece59ccb02a2786292623cea3c2e
Andrei Tupitcyn <andrew.tupitsin@gmail.com>
2017-02-01 17:35:04 +0200

    feat: add support set timestamp as kafka reset offset

--- streaming/src/main/scala/com/github/andr83/parsek/spark/streaming/source/KafkaSource.scala
+++ streaming/src/main/scala/com/github/andr83/parsek/spark/streaming/source/KafkaSource.scala
@@ -3,7 +3,6 @@ package com.github.andr83.parsek.spark.streaming.source
 import java.net.URI
 
 import com.github.andr83.parsek.spark.streaming.StreamingJob
-import com.github.andr83.parsek.spark.streaming.source.KafkaSource.KafkaListener
 import com.github.andr83.parsek.spark.util.HadoopUtils
 import com.github.andr83.parsek.{PString, PValue}
 import com.typesafe.config.Config
@@ -14,12 +13,14 @@ import kafka.serializer.StringDecoder
 import net.ceedubs.ficus.Ficus._
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.streaming.dstream.{DStream, InputDStream}
-import org.apache.spark.streaming.kafka._
+import org.apache.spark.streaming.kafka.{PublicKafkaCluster, _}
 import org.apache.spark.streaming.scheduler._
 import org.apache.spark.streaming.{StreamingContext, StreamingContextState}
 
+import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
+import scala.util.Try
 
 /**
   * @author andr83
@@ -29,7 +30,11 @@ case class KafkaSource(
   topics: Set[String],
   offsetsDir: Option[String] = None,
   reset: Option[String]
+
 ) extends StreamingSource {
+
+  import KafkaSource._
+
   def this(config: Config) = this(
     brokers = config.as[String]("brokers"),
     topics = config.as[Set[String]]("topics"),
@@ -39,7 +44,23 @@ case class KafkaSource(
 
   def apply(job: StreamingJob): DStream[PValue] = {
     val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers) ++
-      reset.map(v => Map("auto.offset.reset" -> v)).getOrElse(Map.empty[String, String])
+      reset.filter(Seq("smallest", "largest").contains).map(v => Map("auto.offset.reset" -> v)).getOrElse(Map.empty[String, String])
+
+    val kc = new PublicKafkaCluster(kafkaParams)
+    val partitions = kc.getPartitions(topics).fold(
+      kafkaErrorHandler(),
+      res => res
+    )
+
+    val partitionsFromOffsetOpt: Option[Map[TopicAndPartition, Long]] = reset.flatMap(r => Try(r.toLong).toOption).map(ts => {
+      kc.getLeaderOffsets(partitions, ts).fold(
+        kafkaErrorHandler(),
+        res => res.mapValues(o => o.offset)
+      )
+    })
+
+    val leaderPartitions: Map[TopicAndPartition, Long] =
+      kc.getLatestLeaderOffsets(partitions).fold(kafkaErrorHandler(), res=> res).mapValues(o=> o.offset)
 
     offsetsDir match {
       case Some(dir) =>
@@ -51,6 +72,7 @@ case class KafkaSource(
         }
 
         def readOffsets(job: StreamingJob, dir: String): Map[TopicAndPartition, Long] = {
+          leaderPartitions ++
           job.listFilesOnly(dir, Seq.empty)
             .map(file => {
               val line = HadoopUtils.readString(file, fs)
@@ -68,33 +90,30 @@ case class KafkaSource(
           KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
             job.ssc, kafkaParams, topics)
         } else {
-          val kc = new PublicKafkaCluster(kafkaParams)
+          val fromOffset = partitionsFromOffsetOpt.getOrElse(leaderPartitions ++ readOffsets(job, dir))
+          logger.info(s"Started kafka stream from offsets: $fromOffset")
 
-          val result = for {
-            topicPartitions <- kc.getPartitions(topics).right
-            leaderOffsets <- kc.getLatestLeaderOffsets(topicPartitions).right
-          } yield leaderOffsets
-
-          result match {
-            //on Kafka getting offsets failed fallback to read string from now
-            case Left(errs) =>
-              logger.error(errs mkString "\n")
-              KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](job.ssc, kafkaParams, topics)
-
-            case Right(topicOffsets) =>
-              val fromOffset = topicOffsets.mapValues(_.offset) ++ readOffsets(job, dir)
-              logger.info(s"Started kafka stream from offsets: $fromOffset")
-
-              val messageHandler = (mmd: MessageAndMetadata[String, String]) => (mmd.key(), mmd.message())
-              KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder, (String, String)](
-                job.ssc, kafkaParams, fromOffset, messageHandler)
-          }
+          KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder, (String, String)](
+            job.ssc, kafkaParams, fromOffset, messageHandler)
         }
         ds.map { case (k, v) => PString(v) }
       case None =>
-        val ds = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](job.ssc, kafkaParams, topics)
+        val ds = partitionsFromOffsetOpt
+          .map(partitionsFromOffset => {
+            KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder, (String, String)](
+              job.ssc, kafkaParams, partitionsFromOffset, messageHandler)
+          })
+          .getOrElse(KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](job.ssc, kafkaParams, topics))
+
         ds.map { case (k, v) => PString(v) }
     }
+  }
+
+  def kafkaErrorHandler[T](): (ArrayBuffer[Throwable] => T) = errors => {
+    errors.foreach(err => {
+      logger.error(err.getMessage, err)
+    })
+    throw errors.head
   }
 }
 
@@ -143,5 +162,7 @@ object KafkaSource {
       })
     }
   }
+
+  val messageHandler: (MessageAndMetadata[String, String] => (String, String)) = (mmd: MessageAndMetadata[String, String]) => (mmd.key, mmd.message)
 
 }
